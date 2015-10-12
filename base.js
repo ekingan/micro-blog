0b97aae12dd86470fea9a42ccd7fe71327e11d5b
ekingan <ekingan@gmail.com>
2015-10-11 21:55:26 -0700

    Working blog 2

--- base.js
+++ base.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
 	
 	$('button').on('click', function(event){    //on click of button
 		event.preventDefault();  //prevents default
-		var input = $('#write-post').val(); //makes a variable out of the input 
-		$('ul').prepend('<li class="list-unstyled">    ' + input + '</li>'); //appends ul with entry
+		var post = $('#write-post').val(); //makes a variable out of the input 
+		$('ul').prepend('<li class="list-unstyled">' + post + '</li>');
 
 		count = count + 1;
    	console.log(count);
@@ -26,8 +26,19 @@ $(document).ready(function(){
  			$('h5').text("There has been " + count + " blog post");	
  		} else {
  		$('h5').text("There have been " + count + " blog posts");
+
+
  }
 };
+	$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+	// var Modal = function (){
+	// 	this.posts =  posts;
+	// 	localStorage = [];
+	// };
 
+	// Modal.posts.push(input);	
+	// console.log(localStorage);
  	
 });
